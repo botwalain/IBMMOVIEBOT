edb6a8de7991cb1dc5c2847a7e4836ba56f8de30
botwalain <167398125+botwalain@users.noreply.github.com>
2024-07-13 20:40:21 +0530

    Update info.py

--- info.py
+++ info.py
@@ -68,7 +68,7 @@ OWNER_USERNAME = environ.get('OWNER_USERNAME', 'simpshh') # owner username witho
 # Clone Information : If Clone Mode Is True Then Bot Clone Other Bots.
 CLONE_MODE = bool(environ.get('CLONE_MODE', True)) # Set True or False
 CLONE_DATABASE_URI = environ.get('CLONE_DATABASE_URI', "mongodb+srv://vicky4:vicky2003@cluster0.ixhdyed.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0") # Necessary If clone mode is true
-PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', 'vggghggjhh') # Public Channel Username Without @ and Bot Is Admin With Full Right.
+PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', 'jcbe826hahwb8266394jshsb') # Public Channel Username Without @ and Bot Is Admin With Full Right.
 
 # Links
 GRP_LNK = environ.get('GRP_LNK', 'https://t.me/IBMMovieReq')
