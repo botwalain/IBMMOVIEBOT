b99f1fc8c52be565dff8599fb108cceb2414af6b
botwalain <167398125+botwalain@users.noreply.github.com>
2024-07-15 19:27:00 +0530

    Update info.py

--- info.py
+++ info.py
@@ -143,7 +143,7 @@ if 'DYNO' in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("URL", "https://wide-ashlie-movbotbyibm-4d9ebf25.koyeb.app/")
+URL = environ.get("URL", "https://grand-ceil-movbotbyibm-fff3f708.koyeb.app/")
 
 
 # Rename Info : If True Then Bot Rename File Else Not
