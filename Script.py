e39c8d17faa92ee9c0fb83b1457eff2f259cfe53
botwalain <167398125+botwalain@users.noreply.github.com>
2024-07-18 10:46:28 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -4,7 +4,8 @@
 
 
 class script(object):
-    START_TXT = """<b><blockquote>Hᴇʟʟᴏ {}, I'm <a href=https://t.me/{}>{}</a></blockquote>
+    START_TXT = """<b>Hᴇʟʟᴏ {},
+I'm <a href=https://t.me/{}>{}</a>
 I Cᴀɴ Pʀᴏᴠɪᴅᴇ Mᴏᴠɪᴇs, Jᴜsᴛ Aᴅᴅ Mᴇ Tᴏ Yᴏᴜʀ Gʀᴏᴜᴘ Aɴᴅ Eɴᴊᴏʏ 😍</b>"""
 
     CLONE_START_TXT = """<b><blockquote>ʜᴇʟʟᴏ {}, ᴍʏ ɴᴀᴍᴇ <a href=https://t.me/{}>{}</a></blockquote>
